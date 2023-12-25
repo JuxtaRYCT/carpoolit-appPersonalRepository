9951f35bc1e6cfddba7bed2c8859143acc3ecec1
Tanush Golwala <tanushgolwala@gmail.com>
2023-12-25 13:35:05 +0530

    feat: added accept/remove pages

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Accept from './screens/accept';
+import Remove from './screens/remove';
 
 export default function App() {
+  // const riderData = {
+  //   name: 'John Doe',
+  //   email: 'johndoe@example.com',
+  //   phone: '+91 9876543210',
+  //   gender: 'Female',
+  //   age: 22,
+  // };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <Accept riderData={riderData} /> */}
+      {/* <Remove riderData={riderData} /> */}
     </View>
   );
 }
---
+++ screens/remove.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { View, ScrollView, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
+import { Text, Icon } from '@rneui/themed';
+import { Card } from '@rneui/themed';
+import BlankSpacer from 'react-native-blank-spacer';
+
+
+let styles = StyleSheet.create({
+  backbutton: {
+    alignSelf: 'flex-start',
+    padding: 25,
+    paddingTop: 40,
+    paddingLeft: 24,
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 0
+  },
+  outputtext: {
+    // backgroundColor: '#E5D9FF',
+    // rounded: true,
+    color: '#49108B',
+    fontWeight: 'normal',
+    fontFamily: 'Roboto',
+    padding: 8,
+    backgroundColor: '#E5D9FF',
+    borderRadius: 10,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontFamily: 'Roboto',
+    color: '#49108B',
+    paddingLeft: 3
+  },
+  leadingtext: {
+    fontWeight: '700',
+    fontFamily: 'Roboto',
+    color: '#49108B',
+    fontSize: 15,
+    padding: 5,
+    paddingTop: 10,
+  },
+  shadowProp: {
+    shadowOffset: { width: -2, height: 4 },
+    shadowColor: '#171717',
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 10,
+  },
+  dualbox: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    // paddingTop:10
+  },
+  buttons: {
+    borderRadius: 24,
+    padding: 10,
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  }
+
+},);
+
+const Remove = ({ riderData }: { riderData: any }) => {
+  const {
+    name,
+    email,
+    phone,
+    gender,
+    age
+  } = riderData;
+
+  return (
+
+    <>
+      <TouchableOpacity style={styles.backbutton}>
+        <Image source={require('../assets/alc.png')} style={{ width: 42, height: 42 }} />
+      </TouchableOpacity>
+      <View style={[styles.container]}>
+        <Text style={[styles.heading, { fontSize: 38, paddingBottom: 20 }]}>Rider Details</Text>
+        <Card containerStyle={{ borderRadius: 10, ...styles.shadowProp, paddingBottom: 10 }}>
+          <Text style={styles.heading}>Personal Details</Text>
+          <Text style={styles.leadingtext}>Name</Text>
+          <Text style={styles.outputtext}>{name}</Text>
+          <Text style={styles.leadingtext}>Email</Text>
+          <Text style={styles.outputtext}>{email}</Text>
+          <Text style={styles.leadingtext}>Phone</Text>
+          <Text style={styles.outputtext}>{phone}</Text>
+          <View style={{ flexDirection: 'row', paddingTop: 10, alignItems: 'stretch', justifyContent: 'space-evenly' }}>
+            <View style={[styles.dualbox, { width: '50%', padding: 5 }]}>
+              <Text style={styles.leadingtext}>Gender</Text>
+              <Text style={styles.outputtext}>{gender}</Text>
+            </View>
+            <View style={[styles.dualbox, { width: '50%', padding: 5 }]}>
+              <Text style={styles.leadingtext}>Age</Text>
+              <Text style={[styles.outputtext]}>{age}</Text>
+            </View>
+          </View>
+        </Card>
+        <BlankSpacer height={30} />
+        <TouchableOpacity style={[styles.buttons, { backgroundColor: '#7E30E1'}]}>
+          <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold',padding:3 }}>Remove</Text>
+        </TouchableOpacity>
+      </View></>
+
+  );
+};
+
+export default Remove;
