e067a7f3565221af382f8bb0f1340ab26c073777
Yash Raj Singh <yashudps.ys@gmail.com>
2023-12-29 16:10:32 +0530

    feat: added hostjointoggle color in colors.tsx

--- design-system/colors.tsx
+++ design-system/colors.tsx
@@ -6,7 +6,8 @@ const Colors = {
     text: '#49108B',
     declineButton: '#FF0000',
     acceptButton: '#14AE5C',
-    UIbutton: '#7E30E1'
+    UIbutton: '#7E30E1',
+    hostjointoggle: '#E26EE5',
     
   } as const;
   
