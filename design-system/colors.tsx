6b5b014cf49405b0841b781174239ec585559b80
Yash Raj Singh <yashudps.ys@gmail.com>
2023-12-29 16:08:43 +0530

    feat: added colors.tsx in design-system file

---
+++ design-system/colors.tsx
@@ -0,0 +1,14 @@
+const Colors = {
+    primary: '#E5D9FF',
+    secondary: '#7E30E1',
+    accent: '#6B3EA0',
+    background: '#FFFFFF',
+    text: '#49108B',
+    declineButton: '#FF0000',
+    acceptButton: '#14AE5C',
+    UIbutton: '#7E30E1'
+    
+  } as const;
+  
+  export default Colors;
+  
