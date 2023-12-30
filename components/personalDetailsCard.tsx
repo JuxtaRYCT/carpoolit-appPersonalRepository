848b7553292f7a3dd09cc88fdff7af53792b6568
Yash Raj Singh <yashudps.ys@gmail.com>
2023-12-30 11:22:30 +0530

    feat: documentation for personalDetails component

--- components/personalDetailsCard.tsx
+++ components/personalDetailsCard.tsx
@@ -17,6 +17,7 @@ interface PersonalDetails{
 const PersonalDetailsCard: React.FC<PersonalDetails>=({personalInfo})=>{
     return(
         <View style={styles.card}>
+            
             <View>
                 <Text style={styles.cardTitle}>
                     Personal Details
@@ -30,6 +31,7 @@ const PersonalDetailsCard: React.FC<PersonalDetails>=({personalInfo})=>{
                     </Text>
                     <Text style={styles.fieldInfo}>
                         {personalInfo.name}
+                        
                     </Text>
                 </View>
                 <View>
@@ -38,34 +40,42 @@ const PersonalDetailsCard: React.FC<PersonalDetails>=({personalInfo})=>{
                     </Text>
                     <Text style={styles.fieldInfo}>
                         {personalInfo.email}
+                        
                     </Text>
                 </View>
-                <View>
+                <View style={styles.fieldAlign}>
                     <Text style={styles.fieldTitle}>
                         Contact Number
                     </Text>
-                    <Text style={styles.fieldInfo}>
-                        {personalInfo.contactNumber}
-                    </Text>
+                    <View>
+                        <Text style={styles.fieldInfo}>
+                            {personalInfo.contactNumber}
+                        </Text>
+                    </View>
+                    
                 </View>
                 
                 <View style={styles.genderAgeRow}>
-                    <View>
+                    <View style={styles.rowElementAlign}>
                         <Text style={styles.fieldTitle}>
                             Gender
                         </Text>
                         <Text style={styles.fieldInfo}>
                             {personalInfo.gender}
+                            
                         </Text>
                     </View>
                     
-                    <View>
+                    <View style={styles.rowElementAlign}>
                         <Text style={styles.fieldTitle}>
                             Age
                         </Text>
-                        <Text style={styles.fieldInfo}>
-                            {personalInfo.yob}
-                        </Text>
+                        <View style={styles.fieldShape}>
+                            <Text style={styles.fieldInfo}>
+                                {personalInfo.yob}
+                            </Text>
+                        </View>
+                        
                     </View>
                     
                 </View>
@@ -79,37 +89,106 @@ const styles=StyleSheet.create({
 
     card:{
         flexDirection: "column",
-        
+        borderRadius: 12,
+        width: '85%',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.5,
+        shadowRadius: 6, 
+        elevation: 5, 
+        backgroundColor: AppColors.background,
+        padding: 10,
+                
     },
 
     cardTitle:{
         fontSize: 24,
         color:AppColors.text,
         fontWeight: "bold",
-        fontFamily: 'Roboto'
-
+        margin: 10
     },
 
     fieldTitle:{
-        fontSize: 18,
+        fontSize: 17,
         color:AppColors.text,
         fontWeight: "bold",
-        fontFamily: 'Roboto'
-
+        marginLeft:8
+        
     },
 
     fieldInfo:{
         fontSize: 15,
         color:AppColors.accent,
-        backgroundColor: AppColors.primary,
-        borderRadius:10,        
-        width: '80%',
-        padding:5,
-        fontFamily: 'Roboto'
-
+        backgroundColor: AppColors.primary,        
+        padding:12,
+        margin:8,
+        
     },
 
+    fieldAlign:{
+        justifyContent:'flex-start'
+    },
+    
+    fieldShape:{
+        borderColor:AppColors.primary,
+        borderRadius: 10
+    
+    },
+    
     genderAgeRow:{
-        flexDirection: "row"
+        flexDirection: "row",
+        
+    },
+
+    rowElementAlign:{
+        flex:1
     }
-})
+});
+
+export default PersonalDetailsCard;
+
+/*
+This is the App.tsx code for running the card component on the screen, it fetches the data from ridersData.json file.
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import PersonalDetailsCard from './components/personalDetailsCard'
+import personalInfo from './riderData.json'
+
+
+
+interface PersonalData{
+
+      name: string;
+      email: string;
+      contactNumber: string;
+      gender:string;
+      yob:number;
+  }
+
+const App: React.FC = () => {
+  const jsonData: PersonalData[] = personalInfo as unknown as PersonalData[];
+
+  return (
+    <View style={styles.container}>
+      {jsonData.map((cardData, index) => (
+        <PersonalDetailsCard key={index} personalInfo={cardData} />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+});
+
+export default App;
+
+
+
+*/ 
